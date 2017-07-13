2bd0f8c6cd2cbade7da0834913f84754bebe9573
Tomasz Plonka <tomasz.plonka@mbc.net>
2017-07-13 14:30:03 +0400

    vizgh storage returns Viz Engine specific locator as URL

--- vizgh.go
+++ vizgh.go
@@ -5,12 +5,21 @@ import (
 	"encoding/xml"
 	"errors"
 	"io/ioutil"
-	"mime"
+	//"mime"
+	"fmt"
 	"net/http"
 	"net/url"
+	"path"
 	"path/filepath"
 	"strings"
 	"time"
+)
+
+const (
+	GET    = "GET"
+	POST   = "POST"
+	PUT    = "PUT"
+	DELETE = "DELETE"
 )
 
 type VizGHStorage struct {
@@ -18,9 +27,6 @@ type VizGHStorage struct {
 	User       string
 	Pass       string
 	Expiration time.Duration
-}
-
-type VizGHFile struct {
 }
 
 func NewVizGHStorage(url string, folder string, user string, pass string, expireAfter int) *VizGHStorage {
@@ -35,85 +41,85 @@ func NewVizGHStorage(url string, folder string, user string, pass string, expire
 // Save saves a file at the given path; the path is the uuid of a folder
 // we want to save a file to
 func (v *VizGHStorage) Save(name string, file File) error {
-	content, err := file.ReadAll()
+	var result []byte
+
+	fileContent, err := file.ReadAll()
 	if err != nil {
 		return err
 	}
+	// one http client for all the requests
+	client := &http.Client{}
 
 	// upload the image
-	sUrl := assembleSaveUrl(v.BaseURL, v.Location, name)
-
-	client := &http.Client{}
-	req, _ := http.NewRequest("POST", sUrl.String(), bytes.NewReader(content))
-	req.Header.Set("Slug", filepath.Base(name))
-	req.Header.Set("Content-Type", mime.TypeByExtension(filepath.Ext(name)))
-	req.SetBasicAuth(v.User, v.Pass)
-	resp, err := client.Do(req)
-	if err != nil || resp.StatusCode != http.StatusOK {
-		return err
-	}
-	// unmarshal the response to get metadata url
-	result, err := ioutil.ReadAll(resp.Body)
+	sUrl := assembleSaveUrl(v.BaseURL, v.Location)
+	headers := map[string]string{"Content-Type": "image/jpeg", "Slug": trimExt(filepath.Base(name))}
+	result, err = restRequest(client, sUrl.String(), POST, v.User, v.Pass, headers, fileContent)
 	if err != nil {
 		return err
 	}
-	req.Body.Close()
+	// process the response - extract the metadata url
 	img := GHFeed{}
 	err = xml.Unmarshal(result, &img)
 	if err != nil {
 		return err
 	}
-
 	metaUrl := getMetadataUrl(&img)
 	if metaUrl == "" {
 		return errors.New("No metadata url found in VizGH reply")
 	}
 
-	// read metadata
-	req2, err := http.NewRequest("GET", metaUrl, nil)
+	// get metadata from the url
+	result, err = restRequest(client, metaUrl, GET, v.User, v.Pass, map[string]string{}, nil)
 	if err != nil {
 		return err
 	}
-	req2.SetBasicAuth(v.User, v.Pass)
-	resp, err = client.Do(req2)
-	if err != nil || resp.StatusCode != http.StatusOK {
-		return err
-	}
-	defer req2.Body.Close()
-	result, err = ioutil.ReadAll(resp.Body)
 
-	if err != nil {
-		return err
-	}
 	meta := GHPayload{}
 	err = xml.Unmarshal(result, &meta)
 	if err != nil {
 		return err
 	}
-
 	// modify expiration date
 	err = modifyExpirationDate(&meta)
 	if err != nil {
 		return err
 	}
-
 	// marshal the xml containing the metadata
-	output, err := xml.Marshal(&meta)
+	metaJson, err := xml.Marshal(&meta)
 	if err != nil {
 		return err
 	}
-
 	// update the metadata in the VizGH
-	req3, _ := http.NewRequest("PUT", metaUrl, bytes.NewReader(output))
-	req3.Header.Set("Content-Type", "application/vnd.vizrt.payload+xml")
-	req3.SetBasicAuth(v.User, v.Pass)
-	resp, err = client.Do(req3)
-	defer req3.Body.Close()
-	if err != nil || resp.StatusCode != http.StatusOK {
-		return err
+	headers = map[string]string{"Content-Type": "application/vnd.vizrt.payload+xml"}
+	_, err = restRequest(client, metaUrl, PUT, v.User, v.Pass, headers, metaJson)
+	if err != nil {
+		return errors.New("File probably exists")
 	}
 	return nil
+}
+
+func restRequest(client *http.Client, url, method, u, p string, h map[string]string, payload []byte) ([]byte, error) {
+	req, err := http.NewRequest(method, url, bytes.NewReader(payload))
+	if err != nil {
+		return []byte{}, err
 	}
+	for k, v := range h {
+		req.Header.Set(k, v)
+	}
+	req.SetBasicAuth(u, p)
+	resp, err := client.Do(req)
+	if err != nil || resp.StatusCode != http.StatusOK {
+		return []byte{}, errors.New(fmt.Sprintf("Error code: %d, error: %v", resp.StatusCode, err))
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	return body, nil
+}
+
+func trimExt(filename string) string {
+	extension := filepath.Ext(filename)
+	return filename[0 : len(filename)-len(extension)]
+}
 
 func modifyExpirationDate(feed *GHPayload) error {
 	for _, v := range feed.GHField {
@@ -138,9 +144,9 @@ func getMetadataUrl(feed *GHFeed) string {
 }
 
 // url in a form http://server/uuid/name, where uuid describes the folder
-func assembleSaveUrl(base, location, name string) *url.URL {
+func assembleSaveUrl(base, location string) *url.URL {
 	b, _ := url.Parse(base)
-	u, _ := url.Parse(strings.Join([]string{"folder", location, name}, "/"))
+	u, _ := url.Parse(strings.Join([]string{"folder", location}, "/"))
 	return b.ResolveReference(u)
 }
 
@@ -169,10 +175,53 @@ func (v *VizGHStorage) Size(filepath string) int64 {
 }
 
 func (v *VizGHStorage) URL(filename string) string {
-	if v.HasBaseURL() {
-		return strings.Join([]string{v.BaseURL, "file", filename}, "/")
+	// URL here is: IMAGE*/path/file
+	// get location and traverse up until root
+	var result []byte
+	var err error
+	var fullPath, partPath, parentFolderId string
+
+	client := &http.Client{}
+	metaUrl := assembleMetadataUrl(v.BaseURL, v.Location)
+	// get metadata, find parent-id and name
+	for {
+		result, err = restRequest(client, metaUrl.String(), GET, v.User, v.Pass, map[string]string{}, nil)
+		if err != nil {
+			return ""
+		}
+		meta := GHPayload{}
+		err = xml.Unmarshal(result, &meta)
+		if err != nil {
+			return ""
+		}
+		partPath, parentFolderId = findNameParentField(&meta)
+		if parentFolderId == "00000000-0000-0000-0000-000000000000" {
+			break
+		}
+		fullPath = strings.Join([]string{partPath, fullPath}, "/")
+		metaUrl = assembleMetadataUrl(v.BaseURL, parentFolderId)
 	}
-	return ""
+	return strings.Join([]string{"IMAGE*", fullPath, trimExt(filename)}, "")
+}
+
+func findNameParentField(meta *GHPayload) (string, string) {
+	var parId, name string
+	for _, v := range meta.GHField {
+		if v.AttrName == "parent-id" {
+			parId = v.GHValue.Text
+		}
+		if v.AttrName == "name" {
+			name = v.GHValue.Text
+		}
+	}
+	return name, parId
+}
+
+func assembleMetadataUrl(base, folder string) *url.URL {
+	b, _ := url.Parse(base)
+	u, _ := url.Parse(path.Join("metadata", "folder", folder))
+	metaUrl := b.ResolveReference(u)
+	return metaUrl
 }
 
 func (v *VizGHStorage) HasBaseURL() bool {
